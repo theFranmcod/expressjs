5dd6034d113a6487d79acf97e0c1f9b7305530d3
Francisco Dias <francisco.dias@challenge.pt>
2022-07-17 23:15:51 +0100

    cc

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import bodyParser from "body-parser";
 import express from "express";
+var cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3333;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
